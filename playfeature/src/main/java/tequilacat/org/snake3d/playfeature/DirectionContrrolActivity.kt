3bf72c30be9777ef853e76e714f911afd4b64037
svo <alexey.vdovichenko@opencascade.com>
2018-12-30 01:02:25 +0300

    first commit - autogenerated project

---
+++ playfeature/src/main/java/tequilacat/org/snake3d/playfeature/DirectionContrrolActivity.kt
@@ -0,0 +1,12 @@
+package tequilacat.org.snake3d.playfeature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DirectionContrrolActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_direction_contrrol)
+    }
+}
